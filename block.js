13b19a8983e3b102abbcd2f9440a115f21c1432c
brentn75 <brentnicholas75@gmail.com>
2019-05-10 17:39:56 -0600

    Added calculateHash.js

--- block.js
+++ block.js
@@ -1,5 +1,9 @@
 class Block {
   constructor(index,previousHash, timestamp, data, hash) {
-    
+    this.index = index;
+    this.previousHash = previousHash.tostring();
+    this.timestamp = timestamp;
+    this.data = data;
+    this.hash = hash.toString();
   }
 }
